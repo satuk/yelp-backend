34cc33d35e5aca10b5152819fc734d1aa3c9a6b0
NancyAnggraini <doloksaribu.nancy@gmail.com>
2017-06-26 11:50:38 +0200

    userService interface and default

---
+++ src/main/java/com/propulsion/yelp/service/DefaultUserSevice.java
@@ -0,0 +1,43 @@
+package com.propulsion.yelp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.propulsion.yelp.domain.User;
+import com.propulsion.yelp.repository.UserRepository;
+
+@Transactional
+@Service
+public class DefaultUserSevice implements UserService{
+
+	//field:
+	private final UserRepository userRepository;
+
+	@Autowired
+	//constructor:
+	public DefaultUserSevice(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	//METHODS:
+	@Override
+	public void deleteById(Long id) {
+		this.userRepository.deleteById(id);
+	}
+
+	@Override
+	public User save(User user) {
+		return this.userRepository.save(user);
+	}
+
+	@Override
+	public User findByFirstName(String firstName) {
+		return this.userRepository.findByFirstName(firstName);
+	}
+
+	@Override
+	public void updateUserById(String firstName, String lastName, Long Id) {
+		this.userRepository.updateUserById(firstName, lastName, Id);
+	}
+}
