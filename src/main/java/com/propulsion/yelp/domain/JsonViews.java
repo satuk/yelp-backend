949b41c668bb6b1ebed1fd143a424b3d0e3031ee
Satuk Kolan <34@satuk.ch>
2017-06-26 16:43:51 +0200

    created jsonView

---
+++ src/main/java/com/propulsion/yelp/domain/JsonViews.java
@@ -0,0 +1,15 @@
+package com.propulsion.yelp.domain;
+import com.fasterxml.jackson.annotation.JsonView;
+
+/**
+ * Created by satuk on 26.06.17.
+ * Views for use with {@link JsonView @JsonView}.
+ */
+public interface JsonViews {
+    
+    interface Summary {}
+    
+//    interface newRestaurant extends Public {
+//
+//    }
+}
