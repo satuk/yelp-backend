3f53458e49e9f5853dfa85976e87f422e7b52e03
Satuk Kolan <34@satuk.ch>
2017-06-26 16:44:31 +0200

    implemented the api/restaurants

--- src/main/java/com/propulsion/yelp/domain/Restaurant.java
+++ src/main/java/com/propulsion/yelp/domain/Restaurant.java
@@ -1,22 +1,12 @@
 package com.propulsion.yelp.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -24,42 +14,46 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(exclude = "id")
 public class Restaurant {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String name;
-	private String address;
-	private String email;
-	private String phone;
-	private String logo;
-	private String url;
-	@OneToMany(mappedBy="restaurant", cascade = CascadeType.REMOVE)
-	private List<Review> reviews = new ArrayList<>();
-	
-	public Restaurant() {
-	}
-	
-	public Restaurant(Long id, String name, String address, String email, String phone, String logo, String url,
-			List<Review> reviews) {
-		this.id = id;
-		this.name = name;
-		this.address = address;
-		this.email = email;
-		this.phone = phone;
-		this.logo = logo;
-		this.url = url;
-		this.reviews = reviews;
-	}
-	
-	public void addReview(Review review) {
-		reviews.add(review);
-	}
-	
-
-	public Restaurant(String name, String address, String email, String phone, String logo, String url,
-			List<Review> reviews) {
-		this(null,name,address,email,phone,logo,url,reviews);
-	}
-	
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(JsonViews.Summary.class)
+    private Long id;
+    @JsonView({JsonViews.Summary.class, JsonViews.Summary2.class})
+    private String name;
+    @JsonView(JsonViews.Summary.class)
+    private String address;
+    @JsonView(JsonViews.Summary.class)
+    private String email;
+    private String phone;
+    private String logo;
+    @JsonView({JsonViews.Summary.class, JsonViews.Summary2.class})
+    private String url;
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.REMOVE)
+    private List<Review> reviews = new ArrayList<>();
+    
+    public Restaurant() {
+    }
+    
+    public Restaurant( Long id, String name, String address, String email, String phone, String logo, String url,
+                       List<Review> reviews ) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.logo = logo;
+        this.url = url;
+        this.reviews = reviews;
+    }
+    
+    public Restaurant( String name, String address, String email, String phone, String logo, String url,
+                       List<Review> reviews ) {
+        this( null, name, address, email, phone, logo, url, reviews );
+    }
+    
+    public void addReview( Review review ) {
+        reviews.add( review );
+    }
+    
 }
---
+++ src/main/java/com/propulsion/yelp/web/RestRestaurantController.java
@@ -0,0 +1,46 @@
+package com.propulsion.yelp.web;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.propulsion.yelp.domain.JsonViews;
+import com.propulsion.yelp.domain.Restaurant;
+import com.propulsion.yelp.service.DefaultRestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by satuk on 26.06.17.
+ */
+
+@RestController
+@RequestMapping("/api")
+public class RestRestaurantController {
+    
+    private final DefaultRestaurantService restaurantService;
+    
+    
+    @Autowired
+    public RestRestaurantController( DefaultRestaurantService restaurantService ) {
+        this.restaurantService = restaurantService;
+    }
+    
+    @JsonView(JsonViews.Summary.class)
+    @GetMapping("/restaurants")
+    public List<Restaurant> retrieveAllRestaurants() {
+        
+        List<Restaurant> restaurants = restaurantService.findAll();
+        
+        System.out.println( restaurants );
+        System.out.println( restaurants.size() );
+        return restaurants;
+    }
+    
+    @JsonView(JsonViews.Summary2.class)
+    @GetMapping("/restaurants2")
+    public List<Restaurant> test() {
+        return restaurantService.findAll();
+    }
+}
