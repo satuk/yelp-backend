b70a730244e822b7922d6e9d1c90825e971a97be
Satuk Kolan <34@satuk.ch>
2017-06-27 16:01:58 +0200

    defined the json view for detail page

--- src/main/java/com/propulsion/yelp/domain/Restaurant.java
+++ src/main/java/com/propulsion/yelp/domain/Restaurant.java
@@ -17,26 +17,28 @@ public class Restaurant {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @JsonView(JsonViews.Summary.class)
+    @JsonView({JsonViews.Summary.class, JsonViews.Detail.class})
     private Long id;
-    @JsonView(JsonViews.Summary.class)
+    @JsonView({JsonViews.Summary.class, JsonViews.Detail.class})
     private String name;
-    @JsonView(JsonViews.Summary.class)
+    @JsonView({JsonViews.Summary.class, JsonViews.Detail.class})
     private String address;
-    @JsonView(JsonViews.Summary.class)
+    @JsonView({JsonViews.Summary.class, JsonViews.Detail.class})
     private String email;
+    @JsonView({JsonViews.Summary.class, JsonViews.Detail.class})
     private String phone;
+    @JsonView(JsonViews.Detail.class)
     private String logo;
-    @JsonView(JsonViews.Summary.class)
+    @JsonView({JsonViews.Summary.class, JsonViews.Detail.class})
     private String url;
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.REMOVE)
+    @JsonView(JsonViews.Detail.class)
     private List<Review> reviews = new ArrayList<>();
     
     public Restaurant() {
     }
     
-    public Restaurant( Long id, String name, String address, String email, String phone, String logo, String url,
-                       List<Review> reviews ) {
+    public Restaurant( Long id, String name, String address, String email, String phone, String logo, String url, List<Review> reviews ) {
         this.id = id;
         this.name = name;
         this.address = address;
@@ -47,8 +49,7 @@ public class Restaurant {
         this.reviews = reviews;
     }
     
-    public Restaurant( String name, String address, String email, String phone, String logo, String url,
-                       List<Review> reviews ) {
+    public Restaurant( String name, String address, String email, String phone, String logo, String url, List<Review> reviews ) {
         this( null, name, address, email, phone, logo, url, reviews );
     }
     
