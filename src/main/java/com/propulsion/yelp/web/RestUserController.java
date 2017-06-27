a38b0476a62f079da7c5394a7fa8c08883495341
Satuk Kolan <34@satuk.ch>
2017-06-27 17:34:48 +0200

    implemented all users

--- src/main/java/com/propulsion/yelp/web/RestUserController.java
+++ src/main/java/com/propulsion/yelp/web/RestUserController.java
@@ -1,34 +1,40 @@
 package com.propulsion.yelp.web;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.propulsion.yelp.domain.JsonViews;
+import com.propulsion.yelp.domain.User;
+import com.propulsion.yelp.service.DefaultUserSevice;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.propulsion.yelp.service.UserService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class RestUserController {
     
-    private final UserService userService;
+    private final DefaultUserSevice userService;
     
     @Autowired
-    public RestUserController( UserService userService ) {
+    public RestUserController( DefaultUserSevice userService ) {
         this.userService = userService;
+    }
+    
+    @GetMapping
+    @JsonView(JsonViews.Summary.class)
+    public List<User> retrieveAllUsers() {
+        return this.userService.findAll();
     }
     
     //update User anonymous
     @PutMapping("/user/{id}")
     @ResponseStatus(NO_CONTENT)
-    public void updateuser( @RequestBody String firstName, String lastName,
-                            Long userId, @PathVariable Long id ) {
+    public void updateuser( @RequestBody String firstName, String lastName, Long userId, @PathVariable Long id ) {
         userService.updateUserById( firstName, lastName, userId );
     }
 }
