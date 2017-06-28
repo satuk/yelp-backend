98fea8fb51b0b0036508fe12b5f2e49d9f11dbaf
Satuk Kolan <34@satuk.ch>
2017-06-28 11:42:06 +0200

    added a token class

---
+++ src/main/java/com/propulsion/yelp/domain/Token.java
@@ -0,0 +1,16 @@
+package com.propulsion.yelp.domain;
+
+/**
+ * Created by satuk on 28.06.17.
+ */
+public class Token {
+    private String token;
+    
+    public String getToken() {
+        return token;
+    }
+    
+    public void setToken( String token ) {
+        this.token = token;
+    }
+}
