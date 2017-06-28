764366021402566130a4bb0d1caf6ce948ef0b4a
Satuk Kolan <34@satuk.ch>
2017-06-28 18:06:47 +0200

    uncommented user role for now

--- src/main/java/com/propulsion/yelp/domain/Role.java
+++ src/main/java/com/propulsion/yelp/domain/Role.java
@@ -1,33 +1,27 @@
 package com.propulsion.yelp.domain;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-import javax.persistence.*;
-import java.io.Serializable;
-
 /**
  * Created by satuk on 27.06.17.
  */
-@Entity
-@Data
-@EqualsAndHashCode(of = "name")
-public class Role implements Serializable {
-    
-    private static final long serialVersionID = 1L;
-    
-    public static final String ROLE_USER = "ROLE_USER";
-    
-    public static final String ROLE_ADMIN = "ROLE_ADMIN";
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    
-    @Column(unique = true, nullable = false, length = 50)
-    private String name;
-    
-    public Role() {
-        /* default constructor: required by JPA */
-    }
-}
+//@Entity
+//@Data
+//@EqualsAndHashCode(of = "name")
+//public class Role implements Serializable {
+//
+//    private static final long serialVersionID = 1L;
+//
+//    public static final String ROLE_USER = "ROLE_USER";
+//
+//    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    @Column(unique = true, nullable = false, length = 50)
+//    private String name;
+//
+//    public Role() {
+//        /* default constructor: required by JPA */
+//    }
+//}
