f4b70b4de67b1dfd602f5bee82ef80feaa23c1f1
Satuk Kolan <34@satuk.ch>
2017-06-28 17:32:20 +0200

    implemented authentication

--- src/main/java/com/propulsion/yelp/web/WebSecurityConfig.java
+++ src/main/java/com/propulsion/yelp/web/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.propulsion.yelp.web;
 
+import com.propulsion.yelp.security.JWTAuthenticationFilter;
+import com.propulsion.yelp.security.JWTLoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -30,37 +35,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure( HttpSecurity http ) throws Exception {
         http
                 .mvcMatcher( "/api/restaurants" )
-                    .authorizeRequests()
-                        .mvcMatchers( GET, "/api/restaurants/**" ).permitAll()
-                        .mvcMatchers( POST, "/api/restaurants" ).permitAll()
-                        .and()
+                .authorizeRequests()
+                .mvcMatchers( GET, "/api/restaurants/**" ).permitAll()
+                .mvcMatchers( POST, "/api/restaurants" ).permitAll()
+                .and()
                 
                 .mvcMatcher( "/api/users" )
-                    .authorizeRequests()
-                        .mvcMatchers( GET, "/api/users/**" ).permitAll()
-                        .and()
-        
+                .authorizeRequests()
+                .mvcMatchers( GET, "/api/users/**" ).permitAll()
+                .and()
+                
                 .mvcMatcher( "/api/reviews" )
-                    .authorizeRequests()
-                        .mvcMatchers( GET, "/api/reviews/**" ).permitAll()
-                        .and()
-        
+                .authorizeRequests()
+                .mvcMatchers( GET, "/api/reviews/**" ).permitAll()
+                .and()
+                
                 .mvcMatcher( "/api/users/sign_in" )
-                    .authorizeRequests()
-                        .mvcMatchers( POST, "/api/users/sign_in/**" ).permitAll()
-                        .and()
+                .authorizeRequests()
+                .mvcMatchers( HttpMethod.POST, "/api/users/sign_in" ).permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+//                 We filter the api/login requests
+                .addFilterBefore( new JWTLoginFilter( "/api/users/sign_in/**", authenticationManager() ),
+                        UsernamePasswordAuthenticationFilter.class )
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore( new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class )
                 
                 .mvcMatcher( "/**" )
-                    .authorizeRequests()
-                        .mvcMatchers( "/**" ).denyAll()
-                        .and()
+                .authorizeRequests()
+                .mvcMatchers( "/**" ).denyAll()
+                .and()
                 
                 .sessionManagement()
-                    .sessionCreationPolicy( SessionCreationPolicy.STATELESS )
-                    .and()
+                .sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+                .and()
                 
                 .csrf()
-                    .disable()
+                .disable()
                 
                 .httpBasic();
     }
@@ -74,5 +87,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 registry.addMapping( "/**" );
             }
         };
+    }
+    
+    @Override
+    protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
+        // Create a default account
+        auth.inMemoryAuthentication()
+                .withUser( "admin" )
+                .password( "password" )
+                .roles( "ADMIN" );
     }
 }
